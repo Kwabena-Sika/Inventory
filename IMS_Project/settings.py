0601637e6e04d693839cf414f6cfda8e2ebcb50a
Kingsley Somuah <kingsleysomuah123@gmail.com>
2025-12-31 15:50:31 +0000

    changes files

--- IMS_Project/settings.py
+++ IMS_Project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@@xqju!762d+5br&c+9q$6n&d4%e1y2pol-+$+u#jhe75bhv^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn IMS_Project.wsgi

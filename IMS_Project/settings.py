5b834fa31aeacefc25d8651a3d30be336b10a499
Kingsley Somuah <kingsleysomuah123@gmail.com>
2025-12-31 18:06:29 +0000

    new changes

--- IMS_Project/settings.py
+++ IMS_Project/settings.py
@@ -28,6 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+CSRF_TRUSTED_ORIGINS = "https://web-production-0d792.up.railway.app/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 
 # Application definition
